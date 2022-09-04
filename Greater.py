3a48e7fcfe3aea4d1b8351e0bd93363c42def7cc
PatelAman17 <95112912+PatelAman17@users.noreply.github.com>
2022-09-04 14:11:50 +0530

    Add files via upload

---
+++ Greater.py
@@ -0,0 +1,12 @@
+# IF program demo
+
+
+a = input('Enter any number here = ')
+b = input('Enter any number here = ')
+
+if a>b:
+    print('A is greater than B')
+elif b>a:
+    print ('B is greater than A')
+else:
+    print('Both A And B are equal')
---
+++ Sum.py
@@ -0,0 +1,7 @@
+
+
+a = input('Enter your number = ')
+b = input('Enter your number = ')
+sum = float(a) + float(b)
+
+print('The sum of {0} & {1} is {2}'.format(a, b, sum))
